9b835bfc5a4af26f9eeb63571ca06c5addaebbe9
mramoszwenger <mramoszwenger@gmail.com>
2024-03-24 23:50:17 -0300

    desafioDos-MaximilianoRamosZwenger

--- entregas/02.manejoArchivosJS/ProductManager.js
+++ entregas/02.manejoArchivosJS/ProductManager.js
@@ -1,98 +1,92 @@
+const { error } = require('node:console');
+const fs = require('node:fs')
+
 class ProductManager {
-    constructor() {
-        this.products = [];
-        this.productIdCounter = 1;
+    constructor(path) {
+        this.path = path
+        this.init()
     }
 
-    addProduct(product) {
+    static async create(path) {
+        const manager = new ProductManager(path);
+        await manager.init();
+        return manager;
+    }
 
-        // Validar que todos los campos sean obligatorios
-        const requiredFields = ['title', 'description', 'price', 'thumbnail', 'code', 'stock'];
-        const allFields = requiredFields.every(field => product.hasOwnProperty(field) && product[field]);
-        if (!allFields) {
-            console.error('Todos los campos son obligatorios.');
-            return;
+    async init () {
+        this.products = await this.readFile();
+        this.productIdCounter = this.products.reduce((max, product) => Math.max(max, product.id), 0) + 1;
+    }
+
+    readFile = async () => {
+        try {
+            const dataJson = await fs.promises.readFile(this.path, 'utf-8')
+            return JSON.parse(dataJson)
+        } catch(error) {
+            return []
         }
+    }
 
-        // Validar que no se repita el campo "code"
-        const uniqueCode = this.products.every(existingProduct => existingProduct.code !== product.code);
-        if (!uniqueCode) {
-            console.error('El código del producto ya existe.');
-            return;
+    addProduct = async (product) => {
+        try {
+
+            // Validar que todos los campos sean obligatorios
+            const requiredFields = ['title', 'description', 'price', 'thumbnail', 'code', 'stock'];
+            const allFields = requiredFields.every(field => product.hasOwnProperty(field) && (product[field] !== undefined && product[field] !== ''));
+            if (!allFields) {
+                console.error('Todos los campos son obligatorios.');
+                return;
+            }
+
+            // Validar que no se repita el campo "code"
+            const uniqueCode = this.products.every(existingProduct => existingProduct.code !== product.code);
+            if (!uniqueCode) {
+                console.error('El código del producto ya existe.');
+                return;
+            }
+
+            // Asignar un id autoincrementable al producto y agregarlo
+            product.id = this.productIdCounter++;
+            this.products.push(product);
+            console.log('Producto agregado:', product);
+
+            await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, '\t'), 'utf-8')
+
+            return this.products
+
+        } catch(error) {
+            console.error(error)
         }
-
-        // Asignar un id autoincrementable al producto y agregarlo
-        product.id = this.productIdCounter++;
-        this.products.push(product);
-        console.log('Producto agregado:', product);
     }
 
-    getProducts() {
-        return this.products;
+    getProducts = async () => {
+        try {
+            return this.products;
+        } catch(error) {
+            console.error(error)
+        }
     }
 
-    getProductById(id) {
-        const product = this.products.find(product => product.id === id);
-        if (product) {
+    getProductById = async (id) => {
+        try {
+
+            const product = this.products.find(product => product.id === id);
+            if (!product) {
+              return 'Producto no encontrado.'  
+            }
+
             return product;
-        } else {
-            console.error('Producto no encontrado.');
+
+        } catch(error) {
+            console.error(error)
         }
     }
+
+    updateProduct = async (id, productToUpdate) => {}
+
+    deleteProduct = async (id) => {}
 }
 
-
-const productManager = new ProductManager();
-
-// Ejemplos
-
-productManager.addProduct({
-    title: 'Disco Solido SSD 240GB',
-    description: 'SSD 240GB 2.5 SATA III Kingstone A400 lectura hasta 500MB Y escritura hasta 450MB',
-    price: 38840,
-    thumbnail: 'img/ssda400-240gb.jpg',
-    code: 'SSDK240',
-    stock: 722
-});
-
-productManager.addProduct({
-    title: 'Disco Solido SSD 480GB',
-    description: 'SSD 480GB 2.5 SATA III Kingstone A400 lectura hasta 500MB Y escritura hasta 450MB',
-    price: 47790,
-    thumbnail: 'img/ssda400-480gb.jpg',
-    code: 'SSDK480',
-    stock: 137
-});
-
-productManager.addProduct({
-    title: 'Pendrive 64GB',
-    description: 'Kingstone DataTraveler Exodia DTX USB 3.2',
-    price: 9080,
-    thumbnail: 'img/datatravel-64gb.jpg',
-    code: 'PDK64GB',
-    stock: 1394
-});
-
-productManager.addProduct({
-    title: 'Pendrive 128GB',
-    description: 'Kingstone DataTraveler Exodia DTX USB 3.2',
-    price: 14830,
-    thumbnail: 'img/datatravel-128gb.jpg',
-    code: 'PDK128GB',
-    stock: 516
-});
-
-productManager.addProduct({
-    title: 'Pendrive 256GB',
-    description: 'Kingstone DataTraveler Exodia DTX USB 3.2',
-    price: 32700,
-    thumbnail: 'img/datatravel-256gb.jpg',
-    code: 'PDK256GB',
-    stock: 9
-});
-
-// Obtener todos los productos
-console.log('Todos los productos:', productManager.getProducts());
-
-// Obtener un producto por id
-console.log('Producto con id 2:', productManager.getProductById(2));
+module.exports = {
+    ProductManager
+}
---
+++ entregas/02.manejoArchivosJS/app.js
@@ -0,0 +1,69 @@
+const { ProductManager } = require('./ProductManager.js');
+const path = './file/Products.json';
+const products = new ProductManager(path);
+
+const main = async () => {
+
+    const products = await ProductManager.create(path);
+
+    // Agregar producto de prueba
+    const response = await products.addProduct({
+        title: 'Disco Solido SSD 240GB',
+        description: 'SSD 240GB 2.5 SATA III Kingstone A400 lectura hasta 500MB Y escritura hasta 450MB',
+        price: 38840,
+        thumbnail: 'img/ssda400-240gb.jpg',
+        code: 'SSDK240',
+        stock: 722
+    })
+
+    console.log('Se ha agregado el producto:', response)
+
+    // Obtener todos los productos
+    console.log('Lista de productos:', products.getProducts());
+
+    // Obtener un producto por id
+    console.log('Producto con por id:', products.getProductById(2));
+
+}
+
+main()
+
+
+
+/* Ejemplos de productos
+
+    title: 'Disco Solido SSD 240GB',
+    description: 'SSD 240GB 2.5 SATA III Kingstone A400 lectura hasta 500MB Y escritura hasta 450MB',
+    price: 38840,
+    thumbnail: 'img/ssda400-240gb.jpg',
+    code: 'SSDK240',
+    stock: 722
+
+    title: 'Disco Solido SSD 480GB',
+    description: 'SSD 480GB 2.5 SATA III Kingstone A400 lectura hasta 500MB Y escritura hasta 450MB',
+    price: 47790,
+    thumbnail: 'img/ssda400-480gb.jpg',
+    code: 'SSDK480',
+    stock: 137
+
+    title: 'Pendrive 64GB',
+    description: 'Kingstone DataTraveler Exodia DTX USB 3.2',
+    price: 9080,
+    thumbnail: 'img/datatravel-64gb.jpg',
+    code: 'PDK64GB',
+    stock: 1394
+
+    title: 'Pendrive 128GB',
+    description: 'Kingstone DataTraveler Exodia DTX USB 3.2',
+    price: 14830,
+    thumbnail: 'img/datatravel-128gb.jpg',
+    code: 'PDK128GB',
+    stock: 516
+
+    title: 'Pendrive 256GB',
+    description: 'Kingstone DataTraveler Exodia DTX USB 3.2',
+    price: 32700,
+    thumbnail: 'img/datatravel-256gb.jpg',
+    code: 'PDK256GB',
+    stock: 9
+*/
