7dd6974287d8266143a5254d6154333dfcac4cd1
mramoszwenger <mramoszwenger@gmail.com>
2024-04-11 19:36:40 -0300

    primerPreEntrega-MaximilianoRamosZwenger

--- entregas/primerPreEntrega/src/routes/cart.router.js
+++ entregas/primerPreEntrega/src/routes/cart.router.js
@@ -5,10 +5,39 @@ const router = Router();
 const path = './src/file/Cart.json';
 const carts = new CartManager(path);
 
-router.get('/:cid', async (request, response) => {});
-router.post('/', async (request, response) => {});
+router.get('/:cid', async (request, response) => {
+    try {
+        const { cid } = request.params;
+        const cart = await carts.getCart(cid);
+        if (!cart) {
+            return response.status(404).send('Carrito no encontrado');
+        }
+        response.json(cart);
+    } catch(error) {
+        response.status(500).send('Error al obtener el carrito');
+    }    
+});
+
+router.post('/', async (request, response) => {
+    try {
+        const newCart = await carts.createCart();
+        response.json(newCart);
+    } catch(error) {
+        response.status(500).send('Error al crear el carrito');
+    }   
+});
 
 router.post('/:cid/products/:pid', async (request, response) => {
-    const {cid, pid} =request.params
-    const response = await carts.addProductToCart(cid, {product:pid, quantity: 1});
+    try {
+        const { cid, pid } = request.params;
+        const cart = await carts.addProductToCart(cid, { product: pid, quantity: 1 });
+        if (!cart) {
+            return response.status(404).send('Carrito no encontrado');
+        }
+        response.json(cart);
+    } catch (error) {
+        response.status(500).send('Error al agregar producto al carrito');
+    }
 });
+
+export default router;
