bcaffdc8ce6c5197bb25e7eb0f88122f49622daa
mramoszwenger <mramoszwenger@gmail.com>
2024-05-17 17:17:18 -0300

    segundaPreEntrega-MaximilianoRamosZwenger

--- entregas/segundaPreEntrega/src/dao/productsManagerMongo.js
+++ entregas/segundaPreEntrega/src/dao/productsManagerMongo.js
@@ -13,12 +13,12 @@ class ProductManagerMongo {
             }
 
             // Validar que el código del producto no exista en otro producto
-            const existingProduct = await this.Product.findOne({ code: product.code });
+            const existingProduct = await this.productModel.findOne({ code: product.code });
             if (existingProduct) {
                 throw new Error('El código del producto ya existe.');
             }
 
-            const newProduct = new this.Product(product);
+            const newProduct = new this.productModel(product);
             await newProduct.save();
             return newProduct;
         } catch (error) {
@@ -28,7 +28,7 @@ class ProductManagerMongo {
 
     getProducts = async () => {
         try {
-            return await this.Product.find({});
+            return await this.productModel.find({});
         } catch (error) {
             console.error('Error al obtener los productos:', error);
             return [];
@@ -37,7 +37,7 @@ class ProductManagerMongo {
 
     getProductById = async (pid) => {
         try {
-            return await this.Product.findOne({ pid: pid });
+            return await this.productModel.findOne({ pid: pid });
         } catch (error) {
             console.error('Error al obtener el producto por ID:', error);
             return null;
@@ -46,7 +46,7 @@ class ProductManagerMongo {
 
     updateProduct = async (pid, productToUpdate) => {
         try {
-            return await this.Product.findOneAndUpdate({ pid: pid }, productToUpdate, { new: true });
+            return await this.productModel.findOneAndUpdate({ pid: pid }, productToUpdate, { new: true });
         } catch (error) {
             console.error('Error al actualizar el producto:', error);
             return null;
@@ -55,7 +55,7 @@ class ProductManagerMongo {
 
     deleteProduct = async (pid) => {
         try {
-            return await this.Product.findOneAndDelete({ pid: pid });
+            return await this.productModel.findOneAndDelete({ pid: pid });
         } catch (error) {
             console.error('Error al eliminar el producto:', error);
             return null;
