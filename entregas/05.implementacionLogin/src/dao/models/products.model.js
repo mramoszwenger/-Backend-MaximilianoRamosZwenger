249e766c06bd484f30cbaa5a031b22011d03e607
mramoszwenger <mramoszwenger@gmail.com>
2024-06-02 14:48:58 -0300

    desafioCinco-MaximilianoRamosZwenger

--- entregas/05.implementacionLogin/src/dao/models/products.model.js
+++ entregas/05.implementacionLogin/src/dao/models/products.model.js
@@ -1,4 +1,5 @@
 import {Schema, model} from "mongoose";
+import mongoosePaginate from 'mongoose-paginate-v2';
 
 const productsCollection = 'products';
 
@@ -29,5 +30,7 @@ const productSchema = new Schema({
         unique: true
     }
 });
+
+productSchema.plugin(mongoosePaginate)
 
 export const productModel = model(productsCollection, productSchema);
