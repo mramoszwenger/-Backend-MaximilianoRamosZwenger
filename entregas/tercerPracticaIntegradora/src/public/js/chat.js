8146c99ab3dc636257217545f073dbda9cb2832e
mramoszwenger <mramoszwenger@gmail.com>
2024-07-31 21:59:34 -0300

    tercerPracticaIntegradora-MaximilianoRamosZwenger

---
+++ entregas/tercerPracticaIntegradora/src/public/js/chat.js
@@ -0,0 +1,31 @@
+// logger.info('Bienvenidos al chat')
+
+const socket = io()
+
+const input = document.getElementById('message')
+const messageList = document.getElementById('list-message')
+
+input.addEventListener('keyup', evt => {
+    if(evt.key === 'Enter'){
+        socket.emit('mensaje_cliente', input.value)
+        input.value= ''
+    }
+})
+// manager create get
+socket.on('messages_server', data => {
+    logger.info(data)
+})
+
+// socket.emit('message', 'esto es data en forma de string')
+
+// socket.on('socket_individual', data => {
+//     logger.info(data)
+// })
+
+
+// socket.on('para_todos_menos_el_actual', data => {
+//     logger.info(data)
+// })
+// socket.on('eventos_para_todo', data => {
+//     logger.info(data)
+// })
--- entregas/tercerPracticaIntegradora/src/routes/views.router.js
+++ entregas/tercerPracticaIntegradora/src/routes/views.router.js
@@ -22,4 +22,23 @@ router.get('/products', (request, response) => {
   response.render('products');
 });
 
+router.get('/chat', (request, response) => {
+  const { socketServer } = request
+
+  socketServer.on('connection', socket => {
+      logger.info('nuevo cliete conectado')
+   
+      const messages = []
+    
+      socket.on('mensaje_cliente', data => {
+          logger.info(data)
+          messages.push({id: socket.id, messge: data})       
+          socketServer.emit('messages_server', messages)
+      })
+  })
+
+  response.render('chat', {
+      styles: 'chatStyles.css' })
+})
+
 export default router;
